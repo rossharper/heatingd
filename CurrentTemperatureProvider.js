1371b5eb28c488acf7f0061d6c4a5809179b9bb5
Ross Harper <rossharper@users.noreply.github.com>
2015-11-02 20:08:08 +0000

    Extracted current temp provider

---
+++ CurrentTemperatureProvider.js
@@ -0,0 +1,9 @@
+function CurrentTemperatureProvider() {
+    this.getCurrentTemperature = function() {
+        return 19.99;
+    }
+}
+
+module.exports = {
+    CurrentTemperatureProvider : CurrentTemperatureProvider
+}

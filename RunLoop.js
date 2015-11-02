ce0e38adcdc36339df98dbe6831c838594d96d46
Ross Harper <ross@rossharper.net>
2015-11-02 20:05:28 +0000

    Extracted RunLoop

---
+++ RunLoop.js
@@ -0,0 +1,14 @@
+function RunLoop(intervalMs, callback) {
+    this.start = function() {
+        this.intervalTimer = setInterval(callback, intervalMs);
+    }
+
+    this.resetInterval = function() {
+        clearInterval(this.intervalTimer);
+        this.start();
+    }
+}
+
+module.exports = {
+    RunLoop : RunLoop
+}

7baf365991bc0e2c3785bb60567d4271a4b6c8b2
Ross Harper <ross@rossharper.net>
2015-11-02 21:34:34 +0000

    read programme data path arg

--- index.js
+++ index.js
@@ -6,29 +6,46 @@ var RunLoop = require('./RunLoop').RunLoop,
 ;
 
 var DEFAULTS = {
-    INTERVAL_SECONDS : 30,
-    SENSOR_DATA_PATH : "/var/lib/homecontrol/sensordata/temperatureSensors/TA"
+    INTERVAL_SECONDS    : 30,
+    SENSOR_DATA_PATH    : "/var/lib/homecontrol/sensordata/temperatureSensors/TA",
+    PROGRAMME_DATA_PATH : "/var/lib/homecontrol/programdata/"
 };
 
 function usage() {
-    console.log("Usage: node index.js [-i <INTERVAL_IN_SECONDS>] [-sensorpath <SENSOR_DATA_PATH>]");
+    console.log("Usage: ");
+    console.log("  node index.js [-i <INTERVAL_IN_SECONDS>] [-sensorpath <SENSOR_DATA_PATH>]");
+    console.log("       [-programme <PROGRAMME_DATA_PATH>]");
+    console.log("");
     console.log("         -i    the interval between comparing temperatures and calling for heat");
     console.log("-sensorpath    path at which temperature sensor readings can be read");
+    console.log(" -programme    path at which programme/schedule data can be found");
     process.exit();
 }
 
 function parseArgs() {
     var args = {
         updateIntervalSeconds : DEFAULTS.INTERVAL_SECONDS,
-        sensorDataPath : DEFAULTS.SENSOR_DATA_PATH
+        sensorDataPath : DEFAULTS.SENSOR_DATA_PATH,
+        programmeDataPath : DEFAULTS.PROGRAMME_DATA_PATH
     };
 
+    function hasArg(arg) {
+        return process.argv[argi] === arg;
+    }
+
+    function readArgValue() {
+        return process.argv[++argi] || usage();
+    }
+
     for(var argi = 2; argi < process.argv.length; argi++) {
-        if(process.argv[argi] === '-i') {
-            args.updateIntervalSeconds = parseInt(process.argv[++argi]) || usage();
+        if(hasArg('-i')) {
+            args.updateIntervalSeconds = parseInt(readArgValue()) || usage();
         }
-        else if(process.argv[argi] === '-sensorpath') {
-            args.sensorDataPath = process.argv[++argi] || usage();
+        else if(hasArg('-sensorpath')) {
+            args.sensorDataPath = readArgValue();
+        }
+        else if(hasArg('-programme')) {
+            args.programmeDataPath = readArgValue();
         }
         else {
             usage();
@@ -43,6 +60,7 @@ function start(args) {
 
     console.log("Monitoring temperature every " + args.updateIntervalSeconds + " seconds.");
     console.log("Monitoring temperatures from sensor path: " + args.sensorDataPath);
+    console.log("Monitoring programme/schedule data from path: " + args.programmeDataPath);
 
     var heatingControl = new HeatingControl(
         new TargetTemperatureProvider(),

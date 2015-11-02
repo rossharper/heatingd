dcf8b2156d2dca402ca238aa93c9c200b4d9e6a4
Ross Harper <rossharper@users.noreply.github.com>
2015-11-02 19:55:01 +0000

    Basic heating control with hardcoded temps

--- index.js
+++ index.js
@@ -10,23 +10,60 @@ function RunLoop(intervalMs, callback) {
     }
 }
 
-var i = 0;
+function CurrentTemperatureProvider() {
+    this.getCurrentTemperature = function() {
+        return 19.99;
+    }
+}
 
+function TargetTemperatureProvider() {
+    this.getTargetTemperature = function() {
+        return 20.00;
+    }
+}
 
+function CallForHeatOnCommand() {
+    this.execute = function() {
+        console.log("Calling for heat");
+    }
+}
 
-var runloop = new RunLoop(5000, onInterval);
+function CallForHeatOffCommand() {
+    this.execute = function() {
+        console.log("Calling for heat");
+    }
+}
 
-function onInterval() {
-    console.log("onInterval");
-    i++;
-    if(i % 5 == 0) {
-        console.log("resetting interval");
-        runloop.resetInterval();
+function HeatingControl(targetTemperatureProvider, currentTemperatureProvider, callForHeatOnCommand, callForHeatOffCommand) {
+    this.onInterval = function() {
+        if(shouldCallForHeat()) {
+            callForHeatOnCommand.execute();
+        }
+        else {
+            callForHeatOffCommand.execute();
+        }
+    }
+
+    function shouldCallForHeat() {
+        var targetTemp = targetTemperatureProvider.getTargetTemperature();
+        var currentTemp = currentTemperatureProvider.getCurrentTemperature();
+        return (currentTemp < targetTemp); // TODO: hysteresis
     }
 }
 
 function start() {
     console.log("Starting heatingd...");
+
+    var heatingControl = new HeatingControl(
+        new TargetTemperatureProvider(),
+        new CurrentTemperatureProvider(),
+        new CallForHeatOnCommand(),
+        new CallForHeatOffCommand()
+    );
+
+    var runloop = new RunLoop(5000, function() {
+        heatingControl.onInterval();
+    });
     runloop.start();
 }
 start();

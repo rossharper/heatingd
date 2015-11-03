540957dfd76b07f0d3f919cddbf1d94e726dba28
Ross Harper <rossharper@users.noreply.github.com>
2015-11-03 18:43:18 +0000

    refactor: extract programme provider

--- TargetTemperatureProvider.js
+++ TargetTemperatureProvider.js
@@ -1,41 +1,11 @@
-var fs          = require('fs'),
-    Programme    = require('./Programme').Programme;
-
-var DEFAULT_PROGRAMME_FILE = "defaultProgramme.json";
-var PROGRAMME_FILE = "programme.json";
+var ProgrammeProvider   = require('./ProgrammeProvider').ProgrammeProvider;
 
 function TargetTemperatureProvider(programmeDataPath) {
 
-    var programme = new Programme(readProgramme());
-
-    // TODO: watch for schedule file changes
+    var programmeProvider = new ProgrammeProvider(programmeDataPath);
 
     this.getTargetTemperature = function() {
-        return programme.getCurrentTargetTemperature(new Date());
-    }
-
-    function getProgrammeDataFilePath() {
-        return programmeDataPath + "/" + PROGRAMME_FILE;
-    }
-
-    function readProgramme() {
-        var programmeFilePath = getProgrammeDataFilePath();
-        try{
-            return readProgrammeFile(programmeFilePath);
-        }
-        catch(e) {
-            if (e.code === 'ENOENT') {
-                console.log("Programme data file missing: " + programmeFilePath);
-                return readProgrammeFile(DEFAULT_PROGRAMME_FILE);
-            } else {
-                throw e;
-            }
-        }
-    }
-
-    function readProgrammeFile(programmeFilePath) {
-        var file = fs.readFileSync(programmeFilePath, "utf8");
-        return JSON.parse(file);
+        return programmeProvider.getProgramme().getCurrentTargetTemperature(new Date());
     }
 }
 

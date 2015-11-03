976bffa1184b24b65725acb94b1b68fcc91f3953
Ross Harper <ross@rossharper.net>
2015-11-03 21:04:43 +0000

    added switching diff diag

--- HeatingControl.js
+++ HeatingControl.js
@@ -1,3 +1,5 @@
+var SWITCHING_DIFFERENTIAL = 1.00;
+
 function HeatingControl(targetTemperatureProvider, currentTemperatureProvider, callForHeatOnCommand, callForHeatOffCommand) {
     this.onInterval = function() {
         if(shouldCallForHeat()) {
@@ -8,12 +10,30 @@ function HeatingControl(targetTemperatureProvider, currentTemperatureProvider, c
         }
     }
 
+/*
+    /|\ L1 - heating signal
+     |
+  ON |-------|---------|
+     |       |         |
+     |      /|\       \|/
+     |       |    w    |
+ OFF |-------|----|----|----> T[°C]
+             |         |
+             |<--SD--->|
+
+    SD - switching differential (1K)
+    W  - room temperature setpoint
+    T  - room temperatures
+    L1 - Output signal for heating
+*/
+
     function shouldCallForHeat() {
         var currentLessThanTarget = false;
 
         var targetTemp = targetTemperatureProvider.getTargetTemperature();
         var currentTemp = currentTemperatureProvider.getCurrentTemperature();
         console.log("Current temperature is " + currentTemp + "°C - Target temperature is " + targetTemp + "°C");
+
         currentLessThanTarget = (currentTemp < targetTemp); // TODO: hysteresis & switching differential
 
         return currentLessThanTarget;

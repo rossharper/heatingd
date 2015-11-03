7c51ded7845d7211f0adec90ce7ccba9d2cd6380
Ross Harper <ross@rossharper.net>
2015-11-03 19:52:26 +0000

    added todo msg

--- CallForHeatCommandFactory.js
+++ CallForHeatCommandFactory.js
@@ -4,12 +4,14 @@ var callForHeatShellCommand = "";
 function CallForHeatOnCommand() {
     this.execute = function() {
         console.log("Calling for heat ON");
+        // TODO: call for heat!
     }
 }
 
 function CallForHeatOffCommand() {
     this.execute = function() {
         console.log("Calling for heat OFF");
+        // TODO: call for heat off!
     }
 }
 

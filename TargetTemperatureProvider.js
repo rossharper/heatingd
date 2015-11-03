60e0ac0f4e7b6225205c39635deb3c9f54945886
Ross Harper <rossharper@users.noreply.github.com>
2015-11-03 18:22:32 +0000

    extracted schedule

--- TargetTemperatureProvider.js
+++ TargetTemperatureProvider.js
@@ -7,52 +7,12 @@ var DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturda
 
 function TargetTemperatureProvider(programmeDataPath) {
 
-    var schedule = readSchedule();//new Schedule(readSchedule());
+    var schedule = new Schedule(readSchedule());
 
     // TODO: watch for schedule file changes
 
     this.getTargetTemperature = function() {
-        return getTargetTemperatureFromSchedule();
-    }
-
-    function getTargetTemperatureFromSchedule() {
-        return (inAnyComfortPeriodForDate(new Date())) ? schedule.comfortTemp : schedule.setbackTemp;
-    }
-
-    function laterThanComfortPeriodStart(date, period) {
-        var start = getDateFromTimeStr(date, period.startTime);
-        return isFirstDateBeforeSecondDate(start, date);
-    }
-
-    function earlierThanComfortPeriodEnd(date, period) {
-        var end = getDateFromTimeStr(date, period.endTime);
-        return isFirstDateBeforeSecondDate(date, end);
-    }
-
-    function isFirstDateBeforeSecondDate(firstDate, secondDate) {
-        return firstDate.getTime() < secondDate.getTime();
-    }
-
-    function getDateFromTimeStr(date, timeStr) {
-        return new Date(date.getFullYear(), date.getMonth(), date.getDate(), parseInt(timeStr.substr(0,2)), parseInt(timeStr.substr(3,2)), 0);
-    }
-
-    function getDayOfWeek(date) {
-        return DAYS[date.getDay()];
-    }
-
-    function inComfortPeriod(date, comfortPeriod) {
-        return laterThanComfortPeriodStart(date, comfortPeriod) && earlierThanComfortPeriodEnd(date, comfortPeriod);
-    }
-
-    function inAnyComfortPeriodForDate(date) {
-        var periodsForToday = schedule.schedule[getDayOfWeek(date)].comfortPeriods;
-        for(var i = 0; i < periodsForToday.length; ++i) {
-            if(inComfortPeriod(date, periodsForToday[i])) {
-                return true;
-            }
-        }
-        return false;
+        return schedule.getCurrentTargetTemperature(new Date());
     }
 
     function getScheduleDataFilePath() {

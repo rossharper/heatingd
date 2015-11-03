10e6c7a787f2efa4af8876ccd68cc65478302d39
Ross Harper <ross@rossharper.net>
2015-11-03 18:22:32 +0000

    extracted schedule

--- Schedule.js
+++ Schedule.js
@@ -1,6 +1,50 @@
-
+var DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 function Schedule(schedule) {
+    this.getComfortTemperature = function() {
+        return schedule.comfortTemp;
+    }
+    this.getSetbackTemperature = function() {
+        return schedule.setbackTemp;
+    }
+    this.getCurrentTargetTemperature = function(date) {
+        return (inAnyComfortPeriodForDate(date)) ? this.getComfortTemperature() : this.getSetbackTemperature();
+    }
 
+    function laterThanComfortPeriodStart(date, period) {
+        var start = getDateFromTimeStr(date, period.startTime);
+        return isFirstDateBeforeSecondDate(start, date);
+    }
+
+    function earlierThanComfortPeriodEnd(date, period) {
+        var end = getDateFromTimeStr(date, period.endTime);
+        return isFirstDateBeforeSecondDate(date, end);
+    }
+
+    function isFirstDateBeforeSecondDate(firstDate, secondDate) {
+        return firstDate.getTime() < secondDate.getTime();
+    }
+
+    function getDateFromTimeStr(date, timeStr) {
+        return new Date(date.getFullYear(), date.getMonth(), date.getDate(), parseInt(timeStr.substr(0,2)), parseInt(timeStr.substr(3,2)), 0);
+    }
+
+    function getDayOfWeek(date) {
+        return DAYS[date.getDay()];
+    }
+
+    function inComfortPeriod(date, comfortPeriod) {
+        return laterThanComfortPeriodStart(date, comfortPeriod) && earlierThanComfortPeriodEnd(date, comfortPeriod);
+    }
+
+    function inAnyComfortPeriodForDate(date) {
+        var periodsForToday = schedule.schedule[getDayOfWeek(date)].comfortPeriods;
+        for(var i = 0; i < periodsForToday.length; ++i) {
+            if(inComfortPeriod(date, periodsForToday[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 module.exports = {

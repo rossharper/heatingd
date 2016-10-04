b2d8602991ceff7d7499a83ee9745b73bfcd32b2
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 15:59:24 +0100

    delint callforheatcommandfactory

--- CallForHeatCommandFactory.js
+++ CallForHeatCommandFactory.js
@@ -1,23 +1,22 @@
-var sys = require('sys')
-var exec = require('child_process').execSync;
+'use strict';
 
-var callForHeatShellCommand = "";
+const exec = require('child_process').execSync;
 
 function CallForHeatOnCommand() {
-    this.execute = function() {
-        console.log("Calling for heat ON");
-        exec("callforheat 1");
-    }
+  this.execute = function () {
+    console.log('Calling for heat ON');
+    exec('callforheat 1');
+  };
 }
 
 function CallForHeatOffCommand() {
-    this.execute = function() {
-        console.log("Calling for heat OFF");
-        exec("callforheat 0");
-    }
+  this.execute = function () {
+    console.log('Calling for heat OFF');
+    exec('callforheat 0');
+  };
 }
 
 module.exports = {
-    CallForHeatOnCommand : CallForHeatOnCommand,
-    CallForHeatOffCommand : CallForHeatOffCommand
-}
+  CallForHeatOnCommand: CallForHeatOnCommand,
+  CallForHeatOffCommand: CallForHeatOffCommand
+};

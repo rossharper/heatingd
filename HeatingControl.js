182aceb4f5d0f71f2ee436427bfe19191f66a371
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 15:07:03 +0100

    delint heatingcontrol.js

--- HeatingControl.js
+++ HeatingControl.js
@@ -1,74 +1,75 @@
-var SWITCHING_DIFFERENTIAL = 1.00;
+'use strict';
+
+const SWITCHING_DIFFERENTIAL = 1.00;
 
 function HeatingControl(programme, currentTemperatureProvider, callForHeatOnCommand, callForHeatOffCommand) {
 
-    var callingForHeat = undefined;
+  let callingForHeat = undefined;
 
-    this.onInterval = function() {
-        update();
+  this.onInterval = function () {
+    update();
+  };
+
+  this.onProgrammeChanged = function (updatedProgramme) {
+    callingForHeat = undefined;
+    programme = updatedProgramme;
+    update();
+  };
+
+  function update() {
+    if (shouldCallForHeat()) {
+      callForHeatOnCommand.execute();
+      callingForHeat = true;
+    } else {
+      callForHeatOffCommand.execute();
+      callingForHeat = false;
     }
+  }
 
-    this.onProgrammeChanged = function(updatedProgramme) {
-        callingForHeat = undefined;
-        programme = updatedProgramme;
-        update();
+  function getTargetTemperature() {
+    return programme.getCurrentTargetTemperature(new Date());
+  }
+
+  /*
+      Hysteresis
+      ==========
+
+      /|\ L1 - heating signal
+       |
+    ON |-------|---------|
+       |       |         |
+       |      /|\       \|/
+       |       |    w    |
+   OFF |-------|----|----|----> T[°C]
+               |         |
+               |<--SD--->|
+
+      SD - switching differential (1K)
+      W  - room temperature setpoint
+      T  - room temperatures
+      L1 - Output signal for heating
+  */
+  function shouldCallForHeat() {
+    const targetTemp = getTargetTemperature();
+    const currentTemp = currentTemperatureProvider.getCurrentTemperature();
+    console.log(`Current temperature is ${currentTemp}°C - Setpoint is ${targetTemp}°C`);
+
+    const lowPoint = targetTemp - (0.5 * SWITCHING_DIFFERENTIAL);
+    const highPoint = targetTemp + (0.5 * SWITCHING_DIFFERENTIAL);
+
+    if (currentTemp >= highPoint) {
+      console.log('Current temp above hysteresis high point');
+      return false;
     }
-
-    function update() {
-        if(shouldCallForHeat()) {
-            callForHeatOnCommand.execute();
-            callingForHeat = true;
-        }
-        else {
-            callForHeatOffCommand.execute();
-            callingForHeat = false;
-        }
+    if (currentTemp < lowPoint) {
+      console.log('Current temp below hysteresis low point');
+      return true;
     }
-
-    function getTargetTemperature() {
-        return programme.getCurrentTargetTemperature(new Date());
-    }
-
-/*
-    Hysteresis
-    ==========
-
-    /|\ L1 - heating signal
-     |
-  ON |-------|---------|
-     |       |         |
-     |      /|\       \|/
-     |       |    w    |
- OFF |-------|----|----|----> T[°C]
-             |         |
-             |<--SD--->|
-
-    SD - switching differential (1K)
-    W  - room temperature setpoint
-    T  - room temperatures
-    L1 - Output signal for heating
-*/
-    function shouldCallForHeat() {
-        var targetTemp = getTargetTemperature();
-        var currentTemp = currentTemperatureProvider.getCurrentTemperature();
-        console.log("Current temperature is " + currentTemp + "°C - Setpoint is " + targetTemp + "°C");
-
-        var lowPoint = targetTemp - (0.5 * SWITCHING_DIFFERENTIAL);
-        var highPoint = targetTemp + (0.5 * SWITCHING_DIFFERENTIAL);
-
-        if(currentTemp >= highPoint) {
-            console.log("Current temp above hysteresis high point");
-            return false;
-        }
-        if(currentTemp < lowPoint) {
-            console.log("Current temp below hysteresis low point");
-            return true;
-        }
-        console.log("Current temperature within switching differential. Continue calling for heat: " + callingForHeat);
-        return (callingForHeat == undefined) ? true : callingForHeat;
-    }
+    console.log(`Current temperature within switching differential. Continue calling for heat: ${callingForHeat}`);
+    return (callingForHeat === undefined) ? true : callingForHeat;
+  }
 }
 
 module.exports = {
-    HeatingControl : HeatingControl
-}
+  HeatingControl: HeatingControl
+};

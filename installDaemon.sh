5e7363d33440d11f11128b9305a0e01fe74bc99e
rossharper <rossharper@users.noreply.github.com>
2022-01-25 18:57:43 +0000

    Add no-execute parameter to setup script

--- installDaemon.sh
+++ installDaemon.sh
@@ -1,4 +1,4 @@
 #forever-service install heatingd -s index.js --start -r pi
 
-pm2 start index.js  --name heatingd -l ~/homecontrol/logs/heatingd
+pm2 start index.js  --name heatingd -l ~/homecontrol/logs/heatingd -- -x
 pm2 save
